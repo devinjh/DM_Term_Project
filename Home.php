b1e134de3266628050636f80072b8d7e9d0897b2
devinjh <44875825+devinjh@users.noreply.github.com>
2019-10-22 21:28:36 -0400

    Initial commit

---
+++ Home.php
@@ -0,0 +1,21 @@
+<!--
+
+This is the home page for Devin Hopkins and Tristan Hess' database management term project.
+
+-->
+
+<html>
+	<head>
+		<title> Home Page </title>
+		<style type = "text/css">
+		td, th, table {border: thin solid black; }
+		</style>
+	</head>
+<body>
+
+<center><h1>
+	Telecom Logging Crux (TLC)
+</h1></center>
+
+</body>
+</html>
